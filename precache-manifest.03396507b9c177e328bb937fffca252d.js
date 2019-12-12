f7c249121ff0ad3e2751b189d0105285649ee771
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-13 00:48:35 +0530

    Updates

---
+++ precache-manifest.03396507b9c177e328bb937fffca252d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d60f7a17819d05e1c715803e6ae908",
+    "url": "/react-jokes/index.html"
+  },
+  {
+    "revision": "e4b761ac54f727cd60c0",
+    "url": "/react-jokes/static/css/main.0e4df982.chunk.css"
+  },
+  {
+    "revision": "253378f95dbb69b506bd",
+    "url": "/react-jokes/static/js/2.032399e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-jokes/static/js/2.032399e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4b761ac54f727cd60c0",
+    "url": "/react-jokes/static/js/main.61fa7c32.chunk.js"
+  },
+  {
+    "revision": "832f3dcd0e5f1ab6d919",
+    "url": "/react-jokes/static/js/runtime-main.ab6f0c5a.js"
+  }
+]);
